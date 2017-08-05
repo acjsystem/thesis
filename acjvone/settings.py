c33ab278581f11a772b2f8d17d8bf4efd373581d
acjsystem <bscpev42017@gmail.com>
2017-08-05 06:44:46 +0000

    fix static

--- acjvone/settings.py
+++ acjvone/settings.py
@@ -122,9 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
