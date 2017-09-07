9808d6b16b1ca515dfa13a2de529528f27e792e1
acjsystem <bscpev42017@gmail.com>
2017-09-07 19:24:57 +0800

    Update urls.py

--- acjvone/urls.py
+++ acjvone/urls.py
@@ -22,11 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^', include('carsys.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
