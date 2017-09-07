26b40291e2c2249101817c8b9fa721c97b537771
acjsystem <bscpev42017@gmail.com>
2017-09-07 19:27:41 +0800

    Update urls.py

--- acjvone/urls.py
+++ acjvone/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^', include('carsys.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
